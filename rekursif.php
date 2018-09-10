a45c59ff9e28a5054e2c0d79e3d7fb02e34c48dc
Irvin Naufal <42725269+irvinnaufal@users.noreply.github.com>
2018-09-10 18:45:58 +0700

    Update rekursif.php

--- rekursif.php
+++ rekursif.php
@@ -1,10 +1,75 @@
-<?php
+<?php 
+$a = 1;
+$i = 0;
 
-function rekursif($i){
-  echo "$i <br>";
-  $i--;
-  if($i>0)
-    rekursif($i);
-}
-rekursif(5);
+rekrusif(5);
+function rekrusif($n){
+	global $i;
+	rekrusif2($i,$n);
+	echo "<br>";
+	if (--$n > $i) {
+		rekrusif($n);
+	}
+ }
+
+ function rekrusif2($j,$n){
+ 	global $i;
+ 	if ($j > $i - $n) {
+ 		echo "*";
+ 		rekrusif2($j - 1,$n);
+ 	}
+ }
+
+ echo "=============++++=================<br>";
+
+ function Perulangan($i){
+ 	echo "$i";
+ 	if ($i < 1) {
+ 		$i++;
+ 		 Perulangan($i);
+ 	}
+ }
+
+function Perulangan2($j){
+	echo "$j";
+ 	if ($j < 2) {
+ 		$j++;
+ 		Perulangan2($j);
+ 	}
+ }
+
+function Perulangan3($k){
+	echo "$k";
+ 	if ($k < 3) {
+ 		$k++;
+ 		Perulangan3($k);
+ 	}
+ }
+
+function Perulangan4($l){
+	echo "$l";
+ 	if ($l < 4) {
+ 		$l++;
+ 		Perulangan4($l);
+ 	}
+ }
+
+function Perulangan5($m){
+	echo "$m";
+ 	if ($m < 5) {
+ 		$m++;
+ 		Perulangan5($m);
+ 	}
+ }
+
+echo Perulangan($a);
+echo "<br>";
+echo Perulangan2($a);
+echo "<br>";
+echo Perulangan3($a);
+echo "<br>";
+echo Perulangan4($a);
+echo "<br>";
+echo Perulangan5($a);
+ 
 ?>
